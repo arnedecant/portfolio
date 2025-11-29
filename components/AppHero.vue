6c283eaa74c9f64e2b8770a7b41a78dff7858726
Arne Decant <hello@arnedecant.be>
2025-11-29 12:16:22 +0100

    cta

--- components/AppHero.vue
+++ components/AppHero.vue
@@ -27,13 +27,11 @@ const { socials } = useAppConfig()
         </NuxtLink>
       </li>
     </ul>
-    <NuxtLink to="#projects" class="cta">View my projects</NuxtLink>
+    <CtaButton to="#projects" class="hero__cta">View my projects</CtaButton>
   </section>
 </template>
 
 <style scoped lang="scss">
-
-
 
 .hero {
   font-weight: 100;
@@ -43,7 +41,7 @@ const { socials } = useAppConfig()
   .eyebrow {
     font-size: clamp(0.75rem, 1dvw, 1.5rem);
     margin-bottom: 0.75rem;
-    margin-top: clamp(-2.8rem, -5dvw, -3.6rem);
+    margin-top: auto;
   }
 
   h1 {
@@ -62,83 +60,9 @@ const { socials } = useAppConfig()
   }
 }
 
-.cta {
-  --radius-lg: 1rem; // TODO
-  --border-width: 0.25rem;
-  --width: 20rem;
-  --height: 4rem;
-  width: var(--width);
-  height: var(--height);
-  position: relative;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  color: var(--c-text);
-  font-weight: 400;
-	border-radius: var(--radius-lg);
-  overflow: hidden;
-  // border: 1px solid var(--c-text);
-}
-
-.cta:before,
-.cta:after {
-  content: '';
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: var(--width);
-  height: var(--height);
-}
-
-.cta:before {
-  z-index: -1;
-  background-color: black;
-  border-radius: var(--radius-lg);
-  width: calc(var(--width) - var(--border-width));
-  height: calc(var(--height) - var(--border-width));
-  transform: translate(calc(var(--border-width) / 2), calc(var(--border-width) / 2));
-}
-
-.cta:after {
-  --width-gradient: calc(var(--width) + (var(--border-width) * 2));
-  background: linear-gradient(45deg, #FF0099, #00EBEB, #DB00FF);
-  // background-size: 600% 600%;
-  border-radius: var(--radius-lg);
-  height: var(--width-gradient);
-  width: var(--width-gradient);
-  animation: RotateBorder 10s linear infinite;
-  z-index: -2;
-  filter: blur(10px);
-  transition: filter 1s ease-in;
-  margin-top: calc(var(--width-gradient) * -0.5 + var(--border-width));
-  margin-left: calc(var(--border-width) * -2);
-  transform: rotate(0deg) translate(0, -10%);
-  transform-origin: center center;
-  border-radius: 50%;
-}
-
-.cta:hover {
-  text-decoration: none;
-}
-
-.cta:hover::after {
-  filter: blur(20px);
-  backdrop-filter: blur(10px);
-  transition: filter .4s ease-in;
-  animation-play-state: paused;
-  transform: rotate(720deg);
-}
-
-@keyframes AnimateBorder {
-  0% { background-position: 0% 50% }
-  50% { background-position: 100% 50% }
-  100% { background-position: 0% 50% }
-}
-
-@keyframes RotateBorder {
-  0% { transform: rotate(0deg) translate(0, 0) }
-  50% { transform: rotate(360deg) translate(0, 0) }
-  100% { transform: rotate(729deg) translate(0, 0) }
+.hero__cta {
+  margin-top: clamp(2rem, 5dvh, 5rem);
+  margin-bottom: auto;
 }
 
 </style>
---
+++ components/CtaButton.vue
@@ -0,0 +1,75 @@
+<script setup lang="ts">
+
+const props = defineProps<{
+  to: string
+}>()
+
+</script>
+
+<template>
+  <NuxtLink :to="props.to" class="cta-button">
+    <slot />
+  </NuxtLink>
+</template>
+
+<style scoped lang="scss">
+
+.cta-button {
+  --radius-lg: 1rem; // TODO
+  --border-width: 0.1rem;
+  --width: 20rem;
+  --height: 4rem;
+  width: var(--width);
+  height: var(--height);
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: var(--c-text);
+  font-weight: 300;
+  letter-spacing: 0.3em;
+  border-radius: var(--radius-lg);
+  background-color: var(--c-background);
+  // border: 1px solid var(--c-text);
+}
+
+.cta-button:before,
+.cta-button:after {
+  content: '';
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: var(--width);
+  height: var(--height);
+}
+
+.cta-button:after {
+  background: var(--gradient-aurora);
+  width: calc(var(--width) + (var(--border-width) * 2));
+  height: calc(var(--height) + (var(--border-width) * 2));
+  left: calc(var(--border-width) * -1);
+  top: calc(var(--border-width) * -1);
+  background-size: 600% 300%;
+  border-radius: var(--radius-lg);
+  animation: animateBorder 10s linear infinite;
+  z-index: -2;
+  transition: filter 0.3s ease-in;
+  transform-origin: center center;
+}
+
+.cta-button:hover {
+  text-decoration: none;
+}
+
+.cta-button:hover::after {
+  filter: blur(1rem);
+  transition: filter .4s ease-in;
+  transform: rotate(0);
+}
+
+@keyframes animateBorder {
+  0% { background-position: 0% 50% }
+  50% { background-position: 100% 50% }
+  100% { background-position: 0% 50% }
+}
+</style>
